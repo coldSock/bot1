81325daff58567c477815de1680c0c64a26c4979
coldSock <piotrowskitomek960@gmail.com>
2023-11-20 17:44:17 +0100

    pls work

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,7 @@ import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
 export const items = pgTable('items', {
   id: serial('id').primaryKey(),
-  user: varchar('user', { length: 240 }),
+  user: varchar('user', { length: 200 }),
 });
 
 export type Item = typeof items.$inferSelect;
---
+++ drizzle/0004_large_nomad.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "items" ALTER COLUMN "user" SET DATA TYPE varchar(200);
