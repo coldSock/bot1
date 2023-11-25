72e1d33c2e80586fce10c343c8836016b4879013
frozone <piotrowskitomek960@gmail.com>
2023-11-25 13:02:09 +0100

    Delete src directory

--- src/validations/officer.ts
+++
@@ -1,7 +0,0 @@
-import type { ValidationFunctionProps } from 'commandkit';
-
-export default function ({ interaction, commandObj, handler }: ValidationFunctionProps) {
-  if (interaction.user.id !== '507944419954262027') {
-    return true;
-  }
-}
